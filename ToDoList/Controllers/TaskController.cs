dc4131f7e304d2553a050109bd876d1ef08d273b
Зозук Андрей <and_r_ey@bk.ru>
2025-04-22 14:46:46 +0400

    fix: refactor project

---
+++ ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
