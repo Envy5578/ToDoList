f322546884a7dc1b77533ce3622b4e7c266dc89e
Зозук Андрей <and_r_ey@bk.ru>
2025-04-22 14:09:16 +0400

    Добавьте файлы проекта.

---
+++ ToDoList.DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Domain.Entity;
+
+namespace ToDoList.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+        DbSet<TaskEntity> Tasks { get; set; }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,38 @@
+
+
+
+using Microsoft.EntityFrameworkCore;
+using ToDoList.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql();
+});
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
